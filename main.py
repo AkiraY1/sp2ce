afa14b72f87c29873a05545bdc1ac7c1c4ac96f0
Akira <luckyanchor2@gmail.com>
2021-07-24 10:38:40 -0700

    django stuff

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import requests
 #resp = requests.get(url)
 #open('cool.pdf', 'wb').write(resp.content)
 
-url = "https://ipfs.infura.io:5001/api/v0/add?arg=QmSDVs2Quck1UubvPJKmFUjz2gE1UFF8M91JtUHG1dPLie"
+url = "https://ipfs.infura.io:5001/api/v0/add?arg=QmaQiNTTNjKHwTMTPo8T2vxZB7riR4ssBdKRhNrxqvrhf7"
 resp = requests.post(url)
 print(resp)
